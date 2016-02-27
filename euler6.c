56edfe52316d7b37cc9d4e0b267a5a2a2c4f759b
Jason Teitelbaum <jteitelb@mail.uoguelph.ca>
2016-02-27 01:21:34 -0500

    indentation

--- euler6.c
+++ euler6.c
@@ -2,19 +2,19 @@
 
 int main(void)
 {
-	int n = 100;
-	long int sum;
+    int n = 100;
+    long int sum;
     long int squareOfSum;
     long int sumOfSquares;
     long int difference;
 
-	sum = n*(n+1) / 2;
+    sum = n*(n+1) / 2;
     squareOfSum = sum*sum;
     sumOfSquares = n*(n+1)*(2*n+1) / 6;
 
     difference = squareOfSum - sumOfSquares;
-	
-	printf("%d\n", difference);
+    
+    printf("%d\n", difference);
 
-	return 0;
+    return 0;
 }
